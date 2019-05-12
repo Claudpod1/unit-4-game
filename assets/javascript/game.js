62728e78636567df279086f82b38670dbfff1064
Claud Pod <claudiapollinger.cp@gmail.com>
2019-05-12 14:06:09 -0600

    new-files

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+ //global variables 
+var wins = 0;
+var losses = 0; 
+var targetScore = "";
+var yourScore = "";
+var crystalOptions = {
+    crystal1:"red" , 
+    crystal2:"blue", 
+    crystal3:"green",
+    crystal4:"yellow",
+}
+
+
+
+// adding function 
+// updating the score function 
+// assign the value of the crystal 
+// restart fucntion 
+// click event for the score 
+
